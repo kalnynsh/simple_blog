f0e7f73c528c038e703c2166229ea4b351fc88f0
kalnynsh <kda869@yandex.ru>
2017-03-19 19:41:43 +0300

    In blueblog/urls create url(r'^admin/', include(admin.site.urls)), url(r'^$', TemplateView.as_view(..), name='home'), url(r'^new-user/$', UserRegistrationView.as_view(), name='user_registration').

--- blueblog/urls.py
+++ blueblog/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
+from accounts.views import UserRegistrationView
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='base.html'),
+        name='home'),
+    url(r'^new-user/$', UserRegistrationView.as_view(), name='user_registration'),
 ]
