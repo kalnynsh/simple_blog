5d763a3d01dd30ce9141aaa6b744fb2058144afa
kalnynsh <kda869@yandex.ru>
2017-03-20 20:28:29 +0300

    In blog/views create class NewBlogView.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http.response import HttpResponseRedirect
+from django.utils.text import slugify
+from django.views.generic import CreateView
 
-# Create your views here.
+from blog.forms import BlogForm
+
+
+class NewBlogView(CreateView):
+    form_class = BlogForm
+    template_name = 'blog_settings.html'
+
+    def form_valid(self, form):
+        blog_obj = form.save(commit=False)
+        blog_obj.owner = self.request.user
+        blog_obj.slug = slugify(blog_obj.title)
+
+        blog_obj.save()
+        return HttpResponseRedirect(reverse('home'))
