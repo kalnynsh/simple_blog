c33f42ec08eb269cea6e0b02c04d59f7f99ea9ea
kalnynsh <kda869@yandex.ru>
2017-03-20 17:54:24 +0300

    In blueblog/urls add login url.

--- blueblog/urls.py
+++ blueblog/urls.py
@@ -18,11 +18,13 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from accounts.views import UserRegistrationView
+from django.contrib.auth.views import login
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name='base.html'),
         name='home'),
+    url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
     url(r'^new-user/$', UserRegistrationView.as_view(), name='user_registration'),
 ]
