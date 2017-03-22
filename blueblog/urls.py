ce63e309e94cfaeec746f30dbc14289c66b3760a
kalnynsh <kda869@yandex.ru>
2017-03-22 18:37:55 +0300

    In blueblog/urls add url name='update-blog-post'.

--- blueblog/urls.py
+++ blueblog/urls.py
@@ -23,6 +23,7 @@ from blog.views import NewBlogView
 from blog.views import HomeView
 from blog.views import UpdateBlogView
 from blog.views import NewBlogPostView
+from blog.views import UpdateBlogPostView
 
 
 urlpatterns = [
@@ -34,4 +35,5 @@ urlpatterns = [
     url(r'^blog/new/$', NewBlogView.as_view(), name='new-blog'),
     url(r'^blog/(?P<pk>\d+)/update/$', UpdateBlogView.as_view(), name='update-blog'),
     url(r'^blog/post/new/$', NewBlogPostView.as_view(), name='new-blog-post'),
+    url(r'^blog/post/(?P<pk>\d+)/update/$', UpdateBlogPostView.as_view(), name='update-blog-post'),
 ]
