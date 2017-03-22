8cd9733e7c13cd5316d164f2a868b4f59ad5d5e9
kalnynsh <kda869@yandex.ru>
2017-03-22 17:24:41 +0300

    In blog/forms add class BlogForm().

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,13 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ['title', ]
+
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+
+        fields = [
+            'title',
+            'body',
+        ]
